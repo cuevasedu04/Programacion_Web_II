77bd219034edc6ee4b331920fdf6bebfad8a00e3
cuevasedu04 <eduardocuevas84@aragon.unam.mx>
2025-04-02 12:51:43 -0600

    clase 02042025

---
+++ 02042025/app1.js
@@ -0,0 +1,7 @@
+console.log(document);
+console.log(document.title);
+console.log(document.body);
+
+//Seleccionar un objeto especifico de una pagina web
+console.log(document.getElementById("tituloWeb").textContent);
+console.log(document.getElementById("tituloWeb").textContent);
---
+++ 02042025/app2.js
@@ -0,0 +1,11 @@
+//querySelector = sirve para seleccionar elementos utilizando selectores de ccs como id o class
+/* console.log(document.querySelector("h1"));
+console.log(document.querySelector(".text-primary2"));
+console.log(document.querySelector("#tituloWeb3")); */
+
+
+console.log(document.querySelector(".texto"));
+console.log(document.querySelectorAll(".texto"));
+
+console.log(document.querySelector(".contenido .texto2"));
+
